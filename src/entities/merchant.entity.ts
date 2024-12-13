f4ad36c419529a997fc44f5d54670a79e8d11f51
gitveteran <christopherjonson.work@gmail.com>
2024-12-12 22:09:04 -0500

    merchant resolver implementation(fetch, mutation)

--- src/entities/merchant.entity.ts
+++ src/entities/merchant.entity.ts
@@ -1,13 +1,18 @@
 import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
+import { ObjectType, Field, ID } from '@nestjs/graphql';
 
+@ObjectType() // This makes the class a GraphQL Object Type
 @Entity()
 export class Merchant {
+  @Field(() => ID) // Exposes 'id' as a GraphQL field
   @PrimaryGeneratedColumn()
   id: number;
 
+  @Field() // Exposes 'merchant' field to the GraphQL schema
   @Column({ unique: true }) // Ensures no duplicate merchants
   merchant: string;
 
+  @Field() // Exposes 'isBezosRelated' to the GraphQL schema
   @Column({ default: true }) // Defaults to "Bezos-related" when added
   isBezosRelated: boolean;
 }
