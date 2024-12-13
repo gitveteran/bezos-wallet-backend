de523e3513f195cd39e29eca96b291f71f38b084
gitveteran <christopherjonson.work@gmail.com>
2024-12-12 20:33:24 -0500

    configure sqlite DB with TypeORM.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,17 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'sqlite',          // Database type
+      database: 'bezos.sqlite',   // SQLite database file
+      entities: [__dirname + '/**/*.entity{.ts,.js}'], // Path to entities
+      synchronize: true,       // Auto sync DB schema (disable in production)
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
